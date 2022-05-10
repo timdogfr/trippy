c82d3a90f95422d76d0b555e89076eb9ec6c62f7
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-05-10 18:10:11 +0300

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,9 +11,9 @@ function Welcome() {
                     <Image src={L} w={[0, 0, 0, 48, 80]} display={["none", "none", "none", "block"]} rounded="100%" />
                     <VStack spacing={16}>
                         <Box>
-                            <Heading align="center">Welcome to the</Heading><Heading color="accent" align="center" size="4xl">Trippy Turtles Club</Heading>
+                            <Heading align="center">Welcome to the</Heading><Heading color="accent" align="center" size="4xl">The Funky Horse Pub</Heading>
                         </Box>
-                        <Text align="center" w={["80%", "75%", "70%", "70%"]} fontSize={24}>Join  the  Trippiest  Turtles  in  the  NFT  space! We’re  a close-knit  community  of  10,000 tripped-out turtles stored  as  ERC-721  tokens  on the Ethereum compatible  Polygon  network.</Text>
+                        <Text align="center" w={["80%", "75%", "70%", "70%"]} fontSize={24}>Join  the  Funky  Horse  Pub  in  the  NFT  space! We’re  a  community  of  10,000 hand-drawn Funkys  stored  as  ERC-721  tokens  on the Avalanche  network.</Text>
 
                         <Stack direction={["column", "column", "row", "row"]} spacing={8}>
                             {/* <Link target="_blank" rel="noopener noreferrer" href="https://discord.com/invite/TrippyTurtlesClub" _hover={{}}>
@@ -22,7 +22,7 @@ function Welcome() {
                             <Link target="_blank" rel="noopener noreferrer" href="https://twitter.com/_trippyturtles_" _hover={{}}>
                                 <Button variant='solid' p={6} leftIcon={<FaTwitter />} borderRadius={80} bgColor="accent" color="black" borderColor="accent" borderWidth="2px" _focus={{ outline: 0 }} _hover={{ bgColor: "transparent", color: "white" }}>Follow Us</Button>
                             </Link> */}
-                            <Link target="_blank" rel="noopener noreferrer" href="https://mint.trippyturtlesclub.com/" _hover={{ textDecoration: 'none' }}>
+                            <Link target="_blank" rel="noopener noreferrer" href="https://thefunkyhorsemint.com" _hover={{ textDecoration: 'none' }}>
                                 <Button
                                     variant='solid'
                                     p={10}
